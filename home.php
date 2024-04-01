d21dfce9a7bfeda72060b6a118311ad1c05b119d
unknown <paulmateo06@gmail.com>
2024-04-01 12:46:17 -0500

    Hice un coso

---
+++ home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ejemplo PHP</title>
+</head>
+<body>
+
+<?php
+    // Definir una variable
+    $mensaje = "Hola, mundo!";
+
+    // Imprimir el mensaje en el navegador
+    echo "<h1>$mensaje</h1>";
+?>
+
+</body>
+</html>
